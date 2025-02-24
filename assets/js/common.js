742c6d83717217f67cddc86535e8b25944854c42
RepMatLibrary <128691837+RepMatLibrary@users.noreply.github.com>
2025-02-24 06:04:50 +0000

    Deploying to gh-pages from @ RepMatLibrary/repmatlibrary.github.io@a18d1c3872222617f542c47766f9cc3404a96d28 🚀

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,11 +1,23 @@
 $(document).ready(function() {
+    // Click on .abstract link
     $('a.abstract').click(function() {
         $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
         $(this).parent().parent().find(".bibtex.hidden.open").toggleClass('open');
     });
+
+    // Click on .bibtex link
     $('a.bibtex').click(function() {
         $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
         $(this).parent().parent().find(".abstract.hidden.open").toggleClass('open');
     });
+
+    // Click on .details link (same behavior as .abstract)
+    $('a.details').click(function() {
+        $(this).parent().parent().find(".details.hidden").toggleClass('open');
+        $(this).parent().parent().find(".bibtex.hidden.open").toggleClass('open'); // Close bibtex if open
+        $(this).parent().parent().find(".abstract.hidden.open").toggleClass('open'); // Close abstract if open
+    });
+
+    // Remove waves-effect and waves-light classes
     $('a').removeClass('waves-effect waves-light');
 });
